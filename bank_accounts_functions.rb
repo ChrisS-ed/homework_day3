5d1f3bddf4ab963b6cddb93e6aa765bf3de40b5b
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-11-18 17:36:31 +0000

    all done

--- bank_accounts_functions.rb
+++ bank_accounts_functions.rb
@@ -45,4 +45,59 @@ def number_of_bank_accounts()
   ACCOUNTS.length
 end
 
+def first_bank_account_holder()
+  ACCOUNTS.first[:holder_name]
+end
+
+def print_owner_names()
+  for i in ACCOUNTS do
+    print "Holder name: #{i[:holder_name]}"
+    print "\n"
+  end
+end
+
+def total_cash_in_bank()
+  total = 0
+  for i in ACCOUNTS do
+    total += i[:amount]
+  end
+  return total
+end
+
+def last_bank_account_holder()
+  ACCOUNTS[-1][:holder_name]
+end
+
+def total_business_account_cash()
+  total = 0
+  for i in ACCOUNTS do
+    if i[:type] == "business"
+      total += i[:amount]
+    end
+  end
+  return total
+end
+
+def holder_of_largest_account()
+  biggest = 0
+  for i in ACCOUNTS do
+    if i[:amount] > biggest
+      biggest = i[:amount]
+      richest = i[:holder_name]
+    end
+  end
+  return richest
+end
+
+def holder_of_largest_personal_account()
+  biggest = 0
+  for i in ACCOUNTS do
+    if (i[:amount] > biggest) and (i[:type] == "personal")
+      biggest = i[:amount]
+      richest = i[:holder_name]
+    end
+  end
+  return richest
+end
+
 
--- bank_accounts_runner.rb
+++ bank_accounts_runner.rb
@@ -11,24 +11,41 @@ puts "first bank account owner is #{first_bank_owner}"
 puts "\n"
 
 ###print to screen name of all bank account owners
-# puts "name of bank acccount owners"
-# print_owner_names()
-# puts "\n"
+puts "name of bank account owners:"
+print_owner_names()
+puts "\n"
 
 ###total cash in bank
-# total_cash = total_cash_in_bank()
-# puts "total in bank #{total_cash}"
-# puts "\n"
+total_cash = total_cash_in_bank()
+puts "total in bank: #{total_cash}"
+puts "\n"
 
 ###last account owner
+last_bank_owner = last_bank_account_holder()
+puts "last bank account owner is: #{last_bank_owner}"
+puts "\n"
 
 ###average bank account value
+total_cash = total_cash_in_bank()
+number_of_accounts = number_of_bank_accounts()
+average = total_cash / number_of_accounts
+puts "the average bank account value is: #{average}"
+puts "\n"
 
 ###total cash in business accounts
+total_business_cash = total_business_account_cash()
+puts "total in bank's business accounts: #{total_business_cash}"
+puts "\n"
 
 ###holder of largest bank account
+richest = holder_of_largest_account()
+puts "the holder of the largest bank account is: #{richest}"
+puts "\n"
 
 ###holder of largest personal account
+richest_personal = holder_of_largest_personal_account()
+puts "the holder of the largest personal bank account is: #{richest_personal}"
+puts "\n"
 
 ###any other functionality you want to add.
 
